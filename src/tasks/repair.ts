c1fc570ff2a08138fe2be6c287b573cd9b6a496f
bstriker <StrikeForceZero@gmail.com>
2016-08-22 01:19:10 -0400

    fix whitespace

--- src/tasks/repair.ts
+++ src/tasks/repair.ts
@@ -11,22 +11,22 @@ export default function runRepairTask(creep: CreepWrapper) {
     });
 
     const priorityTargets = priorityBuildAreas
-        .map(flag => flag.pos.findInRange<Structure>(FIND_STRUCTURES, 2, { filter: structure => structure.hits < structure.hitsMax }))
+        .map(flag => flag.pos.findInRange<Structure>(FIND_STRUCTURES, 2, {filter: structure => structure.hits < structure.hitsMax}))
         .filter(x => x.length > 0);
 
     const targets = priorityTargets.length > 0 ? priorityTargets[0] : creep.room.find<Structure>(FIND_STRUCTURES, {
         filter: (structure) => structure.hits < structure.hitsMax,
     });
 
-    if (targets.length > 0){
+    if (targets.length > 0) {
         let currentIndex = 0;
-        let result = creep.repair(targets[currentIndex]);
+        let result       = creep.repair(targets[currentIndex]);
 
         while (currentIndex < targets.length && [ERR_NO_PATH, ERR_INVALID_TARGET].includes(result)) {
             result = creep.repair(targets[++currentIndex]);
         }
 
-        if(result === OK) {
+        if (result === OK) {
             if (creep.isEmpty) {
                 return RoleTaskStatus.Completed;
             }
