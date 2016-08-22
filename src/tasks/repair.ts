609f35c2ba80b7e5db8faa83d1eb7a01ee12ac71
bstriker <StrikeForceZero@gmail.com>
2016-08-21 23:52:29 -0400

    add ability for priority repairs

--- src/tasks/repair.ts
+++ src/tasks/repair.ts
@@ -6,15 +6,32 @@ export default function runRepairTask(creep: CreepWrapper) {
         return RoleTaskStatus.Failed;
     }
 
-    const targets = creep.room.find<Structure>(FIND_STRUCTURES, {
+    const priorityBuildAreas = creep.room.find<Flag>(FIND_FLAGS, {
+        filter: flag => flag.name === 'priority_build',
+    });
+
+    const priorityTargets = priorityBuildAreas
+        .map(flag => flag.pos.findInRange<Structure>(FIND_STRUCTURES, 2, { filter: structure => structure.hits < structure.hitsMax }))
+        .filter(x => x.length > 0);
+
+    const targets = priorityTargets.length > 0 ? priorityTargets[0] : creep.room.find<Structure>(FIND_STRUCTURES, {
         filter: (structure) => structure.hits < structure.hitsMax,
     });
 
-    if (targets.length > 0 && creep.repair(targets[0]) === OK) {
-        if (creep.isEmpty) {
-            return RoleTaskStatus.Completed;
+    if (targets.length > 0){
+        let currentIndex = 0;
+        let result = creep.repair(targets[currentIndex]);
+
+        while (currentIndex < targets.length && [ERR_NO_PATH, ERR_INVALID_TARGET].includes(result)) {
+            result = creep.repair(targets[++currentIndex]);
         }
-        return RoleTaskStatus.Ok;
+
+        if(result === OK) {
+            if (creep.isEmpty) {
+                return RoleTaskStatus.Completed;
+            }
+            return RoleTaskStatus.Ok;
+        }
     }
     return RoleTaskStatus.Failed;
 };
